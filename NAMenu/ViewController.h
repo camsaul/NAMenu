44e43bfd41f014774d51e41aa27bccd8629f2c97
Cameron Saul <camerons@Camerons-MacBook-Pro.local>
2012-02-20 18:14:44 -0800

    Initial Commit

---
+++ NAMenu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NAMenu
+//
+//  Created by Cameron Saul on 2/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ NAMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NAMenu
+//
+//  Created by Cameron Saul on 2/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
